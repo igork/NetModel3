5846bd370409fdc9c4f17d0900bab41dbea09a64
ikourski001 <ikourski@gmail.com>
2020-02-10 20:53:00 -0500

    mongodb connection

--- src/main/java/mongo/Server.java
+++ src/main/java/mongo/Server.java
@@ -1,17 +1,34 @@
 package mongo;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.descending;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
 
     public static void main(String[] arg){
 
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
 
         MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://<username>:<password>@cluster0-ddiwx.mongodb.net/test?retryWrites=true&w=majority");
+                "mongodb+srv://user:user@cluster0-ddiwx.mongodb.net/test?retryWrites=true&w=majority");
 
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase database = mongoClient.getDatabase("test");
@@ -30,15 +47,34 @@ public class Server {
         MongoClient mongoClient = new MongoClient( uri);
 */
 
-        System.out.println(uri);
+        System.out.println(uri.toString());
         //MongoDatabase database = mongoClient.getDatabase("test");
         for (String name : database.listCollectionNames()) {
             System.out.println(name);
         }
-/*
 
-        MongoCollection coll = db.getCollection("test");
+        MongoCollection coll = database.getCollection("test");
 
-        */
+        Document doc = new Document("_id",new ObjectId());
+        //BasicDBObject doc= new BasicDBObject();
+        doc.put("user","tester");
+        doc.put("time",(new Date()).toString());
+        doc.put("userid",100);
+
+        String json = "{'json':'yes'}";
+
+        coll.insertOne(doc);
+
+        //mongodb/blog/post/quick-start-java-and-mongodb
+
+        FindIterable itTesters = coll.find(gte("userid",100));//.into(new ArrayList<>());
+        List<Document> list = (List<Document>) itTesters.into(new ArrayList<>());
+
+        int num = 0;
+        for(Document per:list){
+            System.out.println(num++ + ". " + per.toJson());
+        }
+
+
     }
 }
